c52006bcd55facdb61a093583a7904b09f6b3e4f
Florian Braune <fbraune@1-B-26.42heilbronn.de>
2025-07-29 22:50:41 +0200

    init structure

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,15 @@
 /*   By: fbraune <fbraune@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 22:48:36 by fbraune           #+#    #+#             */
-/*   Updated: 2025/07/29 22:48:40 by fbraune          ###   ########.fr       */
+/*   Updated: 2025/07/29 22:50:34 by fbraune          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int main(int argc, char **argv)
+{
+
+    // Initialize the philosopher simulation here
+    // This is where you would parse arguments, create threads, etc.
+
+    return 0;
+}
