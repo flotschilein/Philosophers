6548704cc5f7b2e8e60d5f6701ce0b89ee7c68ea
Florian Braune <fbraune@1-E-5.42heilbronn.de>
2025-08-01 15:19:15 +0200

    logic start

--- philo/main.c
+++ philo/main.c
@@ -6,15 +6,42 @@
 /*   By: fbraune <fbraune@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 22:48:36 by fbraune           #+#    #+#             */
-/*   Updated: 2025/07/29 22:50:34 by fbraune          ###   ########.fr       */
+/*   Updated: 2025/08/01 15:18:56 by fbraune          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(int argc, char **argv)
+#include "philo.h"
+#include <unistd.h>
+#include <stdio.h>
+
+void call_error(int err)
+{
+	if (err == 1)
+		printf("use with proper arguments");
+}
+void init_philo_perm(char **av)
 {
 
-    // Initialize the philosopher simulation here
-    // This is where you would parse arguments, create threads, etc.
+}
 
-    return 0;
+void init_philo_amount(char **av)
+{
+
+}
+bool input_fail(char **av)
+{
+
+}
+int	main(int ac, char **av)
+{
+	if (ac == 5 && !input_fail(av))
+		init_philo_perm(av);
+	else if (ac == 6 && !input_fail(av))
+		init_philo_amount(av);
+	else
+	{
+		call_error(1);
+		return (1);
+	}
+	return (0);
 }
