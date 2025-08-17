5cd6054b1a838d5459f37a94235ef0fd33b98e61
Florian Braune <fbraune@1-D-11.42heilbronn.de>
2025-08-17 20:50:44 +0200

    i had the issue of times sometimes sometimes decreasing

--- philo/general_utils.c
+++ philo/general_utils.c
@@ -6,7 +6,7 @@
 /*   By: fbraune <fbraune@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/17 17:30:13 by fbraune           #+#    #+#             */
-/*   Updated: 2025/08/17 18:48:38 by fbraune          ###   ########.fr       */
+/*   Updated: 2025/08/17 20:48:53 by fbraune          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ void	print_logs(t_philo *philo, char *msg)
 {
 	long long	local_time;
 
-	local_time = get_cur_time() - philo->table->start_time;
 	pthread_mutex_lock(&philo->table->death_lock);
 	pthread_mutex_lock(&philo->table->write_lock);
+	local_time = get_cur_time() - philo->table->start_time;
 	if (philo->table->shall_die)
 	{
 		if (philo->table->first_death)
