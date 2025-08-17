8a9160467b34619655acda6a3787d1ed40f9f465
Florian Braune <fbraune@1-D-11.42heilbronn.de>
2025-08-17 17:10:52 +0200

    moved time utils

--- philo/main.c
+++ philo/main.c
@@ -6,35 +6,11 @@
 /*   By: fbraune <fbraune@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 22:48:36 by fbraune           #+#    #+#             */
-/*   Updated: 2025/08/17 17:06:37 by fbraune          ###   ########.fr       */
+/*   Updated: 2025/08/17 17:09:48 by fbraune          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
-
-long long	get_cur_time(void)
-{
-	struct timeval	tv;
-	long long		time;
-
-	time = 0;
-	gettimeofday(&tv, NULL);
-	time = tv.tv_sec * 1000;
-	time += tv.tv_usec / 1000;
-	return (time);
-}
-
-void	sleep_n_ms(long long ms)
-{
-	long long	start_time_local;
-
-	start_time_local = get_cur_time();
-	while ((get_cur_time() - start_time_local) < ms - 1)
-		usleep(1000);
-	while ((get_cur_time() - start_time_local) < ms)
-		usleep(100);
-}
 
 void	print_logs(t_philo *philo, char *msg)
 {
